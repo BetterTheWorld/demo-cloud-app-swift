d7569a174621e8fd98c05ea220195912b9e2e96b
Noroboto <danieldiazg01@gmail.com>
2023-07-10 12:19:29 -0400

    Initial Commit

---
+++ DemoCloudSwiftApp/DemoCloudSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCloudSwiftAppApp.swift
+//  DemoCloudSwiftApp
+//
+//  Created by Daniel Diaz on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoCloudSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
