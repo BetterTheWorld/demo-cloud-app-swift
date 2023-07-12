5d6b8d09d91fa14ebeb2ef71447aab35e39c6c44
Noroboto <danieldiazg01@gmail.com>
2023-07-12 11:34:02 -0400

    feat(constants): demo token as env variable

--- DemoCloudSwiftApp/Constants.swift
+++ DemoCloudSwiftApp/Constants.swift
@@ -10,6 +10,11 @@ import Foundation
 struct Constants {
     struct WebView {
         static let javascriptHandler = "flipgiveAppInterface"
-        static let url = "https://cloud.almostflip.com/?token="
+        static let url: String = {
+            guard let token = ProcessInfo.processInfo.environment["FG_DEMO_TOKEN"] else {
+                fatalError("TOKEN environment variable is missing")
+            }
+            return "https://cloud.almostflip.com/?token=\(token)"
+        }()
     }
 }
